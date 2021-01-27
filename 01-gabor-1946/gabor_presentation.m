b8d476f66cc0128249ea0d29109eaea157c30ef9
Sevag Hanssian <sevagh@protonmail.com>
2021-01-27 10:51:39 -0500

    Add great content

--- 01-gabor-1946/gabor_presentation.m
+++ 01-gabor-1946/gabor_presentation.m
@@ -3,11 +3,28 @@ bigWin = 16384;
 
 [x, fs] = audioread('./el_mestizo.wav');
 
+%figure;
+%spectrogram(x,smallWin,smallWin/2,smallWin*2,fs,"yaxis");
+%title("window = 128");
+
+%figure;
+%spectrogram(x,bigWin,bigWin/2,bigWin*2,fs,"yaxis");
+%title("window = 16384");
+
+w1 = gausswin(2048);
+w2 = hann(2048);
+
+%figure;
+%plot(w); hold on; plot(w2); legend('Gaussian window', 'Hann window');
+%title('2048-point windows; Gaussian vs. Hann');
+%xlim([0 2048]);
 
 figure;
-spectrogram(x,smallWin,smallWin/2,smallWin*2,fs,"yaxis");
-title("window = 128");
+smallGauss=gausswin(smallWin);
+spectrogram(x,smallGauss,smallWin/2,smallWin*2,fs,"yaxis");
+title("gausswin = 128");
 
 figure;
-spectrogram(x,bigWin,bigWin/2,bigWin*2,fs,"yaxis");
-title("window = 16384");
+bigGauss=gausswin(bigWin);
+spectrogram(x,bigGauss,bigWin/2,bigWin*2,fs,"yaxis");
+title("gausswin = 16384");
